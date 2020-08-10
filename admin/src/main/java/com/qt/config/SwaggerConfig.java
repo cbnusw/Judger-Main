2fe2f6e44de905c96166ea2f1895435f1cc2a1d1
YongGyunKwon <ygkwon50@gmail.com>
2020-08-10 14:45:19 +0900

    API 문서화 필요한것들!!

---
+++ admin/src/main/java/com/qt/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.qt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiOnfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
