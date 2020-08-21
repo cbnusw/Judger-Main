2cd517b8755010c7aff7a3ce26d1ffdb595881d5
BIINNNN <seobin99@gmail.com>
2020-08-21 21:54:42 +0900

    axios 인스턴스 생성

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -4,7 +4,9 @@ import Button from '../components/common/Button';
 const DashboardPage = () => {
     return (
         <div>
-            <Button>대시보드</Button>
+            <Button>대시</Button>
+            <h1>hi</h1>
+            <h2>bye</h2>
         </div>
     );   
 };
