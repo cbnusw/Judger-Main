255dfd56bac427c9177cc13b428e23ec1c56fcca
BIINNNN <seobin99@gmail.com>
2020-08-24 14:55:36 +0900

    루트 리듀서에 loading 등록

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 // 루트 리듀서
 import { combineReducers } from 'redux';
 import auth from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
     auth,
+    loading,
 });
 
 export default rootReducer;
