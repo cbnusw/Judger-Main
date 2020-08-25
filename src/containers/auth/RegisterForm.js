3117d488cf8ed3ca3c8f499562a068f8c31c90d9
BIINNNN <seobin99@gmail.com>
2020-08-25 12:36:13 +0900

    username, password 파라미터로 액션 디스패치/사가 API 요청

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import{changeField, initializeForm} from '../../modules/auth';
+import{changeField, initializeForm, register} from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 
@@ -9,8 +9,10 @@ import AuthForm from '../../components/auth/AuthForm';
  */
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth}) =>({
-        form: auth.register
+    const {form, auth, authError } = useSelector(({auth}) =>({
+        form: auth.register, 
+        auth: auth.auth,
+        authError: auth.authError,
     }));
 
     // 인풋 변경 이벤트 핸들러
@@ -28,7 +30,12 @@ const RegisterForm = () => {
     // 폼 등록 이벤트 핸들러
     const onSubmit = e => {
         e.preventDefault();
-        // 구현 예정
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm ) {
+            // TODO: 오류처리
+            return;
+        }
+        dispatch(register({username, password }));
     };
 
     /**
@@ -39,6 +46,19 @@ const RegisterForm = () => {
     useEffect(()=>{
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    // 회원가입 성공과 실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if(auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+        }
+    }, [auth, authError]);
 
     return(
         <AuthForm 
